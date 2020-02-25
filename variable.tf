4c5bda233c742931acb0625038a69e153318882a
susmithachilkuri <46213414+susmithachilkuri@users.noreply.github.com>
2020-02-25 12:23:42 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-2 = "ami-0fc20dd1da406780b"
+  }
+}
